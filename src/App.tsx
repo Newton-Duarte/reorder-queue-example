24d32d59d4d9b9ec2ee2b06f06115f3a2f58e635
Newton Duarte <newton_@hotmail.com>
2025-04-24 09:31:58 -0300

    chore: add initial project files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Card } from './components/ui/card'
+
+export function App() {
+  return (
+    <div className="container">
+      <Card>
+        <h1>Hello World</h1>
+      </Card>
+    </div>
+  )
+}
